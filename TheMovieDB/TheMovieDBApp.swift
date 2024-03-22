e69302cf09028d51c91a21cc00105f8ae7efcc22
jossehblanco <jossehblanco@gmail.com>
2024-03-21 20:21:39 -0600

    Initial Commit

---
+++ TheMovieDB/TheMovieDBApp.swift
@@ -0,0 +1,17 @@
+//
+//  TheMovieDBApp.swift
+//  TheMovieDB
+//
+//  Created by jossehblanco on 21/3/24.
+//
+
+import SwiftUI
+
+@main
+struct TheMovieDBApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
